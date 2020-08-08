993d7fcb7ec10dd6a233cfcc879b329e25222f15
LucestDail <dhtmdgussksm@naver.com>
2020-08-08 17:42:32 +0900

    update JavaStudy JavaCode
    bia Codewars

---
+++ Solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public static boolean validatePin(String pin) {
+    	char temp;
+    	boolean pinCompare = true;
+    	if(pin.length() == 4 || pin.length() == 6) {
+    		for(int i = 0; i<pin.length();i++) {
+    			temp = pin.charAt(i);
+    			if(Character.isDigit(temp) == false) {
+    				pinCompare = false;
+    			}
+    		}
+    	}else{
+    		pinCompare = false;
+    	}
+        return pinCompare;
+    }
+}
