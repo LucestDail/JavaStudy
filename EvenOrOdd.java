ff04f76dae6a81b11960b053f1fbf931efded102
hyun <dhtmdgussksm@naver.com>
2020-08-25 20:54:58 +0900

    JavaStudy Code
    bia Codewars

---
+++ EvenOrOdd.java
@@ -0,0 +1,12 @@
+public class EvenOrOdd {
+    public static String even_or_odd(int number) {
+        //Place code here
+      String Even = "Even";
+      String Odd = "Odd";
+    
+      if(number%2 == 0)
+        return Even;
+      else
+        return Odd;
+    }
+}
