3e1b828219e5b2ffcada6b1cb3c72b6abfd092e9
hyun <dhtmdgussksm@naver.com>
2020-09-16 18:47:44 +0900

    2020.09.16
    2020.09.16, 18:47
    Passed Case update

--- ProgrammersSolutionTest.java
+++ ProgrammersSolutionTest.java
@@ -8,12 +8,12 @@ public class ProgrammersSolutionTest {
 
 	@Test
 	public void SolutionTests() {
-		int[] expectedResult1 = {1,5,3,5,1,2,1,4};
+		int[] expectedResult1 = {2,3,4,5,6,7};
 		int[] expectedResult2 = {2,5,7,9,12};
 		int[] testarray1 = {2,1,3,4,1};
 		int[] testarray2 = {5,0,2,7};
 		System.out.println("****** Solution Test ******");
 		assertTrue(Arrays.equals(expectedResult1, ProgrammersSolution.solved(testarray1)));
-		//assertTrue(Arrays.equals(expectedResult2, ProgrammersSolution.solved(testarray2)));
+		assertTrue(Arrays.equals(expectedResult2, ProgrammersSolution.solved(testarray2)));
 	}
 }
